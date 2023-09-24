9c0ff5fc59f085d547148cfc439745f3f4abacf0
Reto Tschuppert <reto.tschuppert@seantis.ch>
2023-09-23 22:17:27 -0400

    Adding code style and style save views and connect them with code

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ from flask import (
     session,
     url_for,
 )
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import Python3Lexer
+from pygments.styles import get_all_styles
+
 
 app = Flask(__name__)
 
 # retrieve it by secrets.token_hex()
 app.secret_key = 'f766a2a3d4aa8a52c66848e783e510d3ecf1b710c5f9b26d68b7c7fe7dffef96'
-PLACEHOLDER_CODE = ("print('Welcome to Tschuppi's code image generator)\n"
-                    "Please paste your code here..")
+PLACEHOLDER_CODE = ("print('Welcome to Tschuppis code image generator')\n"
+                    "print('Please paste your code here..')")
+DEFAULT_STYLE = 'monokai'
 
 
 @app.route('/', methods=['GET'])
@@ -42,3 +48,30 @@ def reset_session():
     session.clear()
     session['code'] = PLACEHOLDER_CODE
     return redirect(url_for('code'))
+
+
+@app.route('/style', methods=['GET'])
+def style():
+    if session['style'] is None:
+        session['style'] = DEFAULT_STYLE
+
+    formatter = HtmlFormatter(style=session['style'])
+    context = {
+        'message': 'Select Your Style 🎨',
+        'style_definitions': formatter.get_style_defs(),
+        'styles': list(get_all_styles()),
+        'style_bg_color': formatter.style.background_color,
+        'highlighted_code': highlight(
+            session['code'], Python3Lexer(), formatter
+        ),
+    }
+    return render_template("style_selection.html", **context)
+
+
+@app.route('/save_style', methods=['POST'])
+def save_style():
+    if request.form.get('style') is not None:
+        session['style'] = request.form.get('style')
+    if request.form.get('code') is not None:
+        session['code'] = request.form.get('code')
+    return redirect(url_for('style'))
